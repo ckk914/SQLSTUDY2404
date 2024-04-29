72cb6489fcba26dd6d4540a448dff7ea0c74ef41
ckk914 <ckk9114@naver.com>
2024-04-29 12:37:50 +0900

    줄바꿈 수정

--- 4. 연습하기 - human-resource.sql
+++ 4. 연습하기 - human-resource.sql
@@ -3,9 +3,15 @@ SELECT * FROM employees;
 
 -- 실습 1 
 -- 1. employees테이블에서 first_name, last_name, salary를 조회하세요.
- SELECT first_name, last_name, salary FROM employees;
+ SELECT 
+ first_name, last_name, salary
+ FROM employees;
 -- 2. departments테이블에서 department_id, department_name, manager_id, location_id를 조회하세요.
-  SELECT department_id, department_name, manager_id, location_id FROM departments;
+  SELECT department_id,
+  department_name,
+  manager_id,
+  location_id
+  FROM departments;
 -- 3. employees테이블에서 first_name의 별칭을 '이름', salary의 별칭을 '급여'로 조회하세요.
 SELECT first_name AS "이름",
             SALARY AS "급여"
@@ -21,17 +27,20 @@ SELECT first_name AS "이름",
         FROM employees;
 -- 실습 2
 -- 1. employees테이블에서 job_id가 'IT_PROG'인 사원의 first_name, job_id, department_id를 조회하세요.
-SELECT first_name, job_id, department_id
+SELECT 
+first_name, job_id, department_id
 FROM employees
 WHERE job_id= 'IT_PROG'
 ;
 -- 2. employees테이블에서 last_name이 'King'인 사원의 first_name, last_name, hire_date를 조회하세요.
-SELECT first_name, last_name, hire_date
+SELECT 
+first_name, last_name, hire_date
 FROM employees
 WHERE last_name= 'King'
 ;
 -- 3. employees테이블에서 salary가 15000보다 크거나 같은 사원의 first_name, salary, hire_date를 조회하세요.
-SELECT first_name, salary, hire_date
+SELECT
+first_name, salary, hire_date
 FROM employees
 WHERE salary >=15000
 ;
@@ -41,47 +50,58 @@ FROM employees
 WHERE salary BETWEEN 10000 AND 12000
 ;
 -- 5. employees테이블에서 manager_id가 101, 102, 103중에 속하는 사원의 first_name, salary, manager_id를 조회하세요.
-SELECT first_name, salary, manager_id
+SELECT 
+first_name, salary, manager_id
 FROM employees
 WHERE manager_id IN (101,102,103)
 ;
 -- 6. employees테이블에서 job_id가 'IT_PROG', 'FI_MGR'중에 포함되지 않는 사원의 first_name, job_id를 조회하세요.
 SELECT first_name, job_id
 FROM employees
-WHERE job_id != 'IT_PROG' AND Job_id != 'FI_MGR'
+WHERE job_id NOT IN ('IT_PROG', 'FI_MGR')
 ;
 -- 7. employees테이블에서 job_id가 'IT'로 시작하는 사원의 first_name, last_name, job_id를 조회하세요.
-SELECT first_name, job_id
+SELECT 
+first_name, job_id
 FROM employees
 WHERE job_id like 'IT%'
 ;
 -- 8. employees테이블에서 email의 두번째 글자가 A인 사원의 first_name, email을 조회하세요.
-SELECT first_name, email
+SELECT
+first_name, email
 FROM employees
 WHERE email like '_A%'
 ;
 -- 9. employees테이블에서 manager_id가 null인 사원의 first_name, manager_id를 조회하세요.
-SELECT first_name, manager_id
+SELECT 
+first_name, manager_id
 FROM employees
 WHERE manager_id IS NULL
 ;
 -- 10. employees테이블에서 commission_pct가 null이 아닌 사원의 first_name, job_id, commission_pct를 조회하세요.
-SELECT first_name, job_id, commission_pct
+SELECT
+first_name, job_id, commission_pct
 FROM employees
 WHERE commission_pct IS NOT NULL
 ;
 -- 11. employees테이블에서 job_id가 'IT_PROG'이고 salary가 5000이상인 사원의 first_name, job_id, salary를 조회하세요.
-SELECT first_name, job_id, salary
+SELECT
+first_name, job_id, salary
 FROM employees
-WHERE job_id = 'IT_PROG' AND salary >= 5000
+WHERE job_id = 'IT_PROG'
+AND salary >= 5000
 ;
 — 12. employees테이블에서 job_id가 'IT_PROG'이거나 salary가 5000이상인 사원의 first_name, job_id, salary를 조회하세요.
-SELECT first_name, job_id, salary
+SELECT 
+first_name, job_id, salary
 FROM employees
-WHERE job_id = 'IT_PROG' OR salary >= 5000
+WHERE job_id = 'IT_PROG' 
+OR salary >= 5000
 ;
 — 13. employees테이블에서 job_id가 'IT_PROG'이거나 'FI_MGR'이면서 salary가 5000이상인 사원의 first_name, job_id, salary를 조회하세요.
-SELECT first_name, job_id, salary
+SELECT 
+first_name, job_id, salary
 FROM employees
-WHERE (job_id = 'IT_PROG' OR job_id = 'FI_MGR') AND salary >= 5000
+WHERE (job_id = 'IT_PROG' OR job_id = 'FI_MGR')
+AND salary >= 5000
 ;
